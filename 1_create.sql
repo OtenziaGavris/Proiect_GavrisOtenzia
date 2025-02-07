d87cc95fff0e84392b1ba28dbfd6a396b48eb2c1
OtenziaGavris <otenzia.gavris03@e-uvt.ro>
2025-02-07 02:39:49 +0200

    Create_update

---
+++ 1_create.sql
@@ -0,0 +1,72 @@
+CREATE TABLE IF NOT EXISTS padure
+(
+	id SERIAL PRIMARY KEY,
+	suprafata DECIMAL NULL,
+	judet VARCHAR (50) NOT NULL,
+	tip VARCHAR (50),
+	id_proprietar INT,
+	FOREIGN KEY (id) REFERENCES proprietar (id) 
+);
+
+CREATE TABLE IF NOT EXISTS proprietar 
+(
+	id SERIAL PRIMARY KEY,
+	nume VARCHAR (100) NOT NULL,
+	adresa VARCHAR (300),
+	telefon VARCHAR (15)
+);
+
+CREATE TABLE IF NOT EXISTS parcela
+(
+	id SERIAL PRIMARY KEY,
+	suprafata DECIMAL,
+	tip_sol VARCHAR (50),
+	an_infiintare INT,
+	FOREIGN KEY (id) references padure (id)
+);
+
+CREATE TABLE IF NOT EXISTS arbore 
+(
+	id SERIAL PRIMARY KEY,
+	specie VARCHAR (50),
+	varsta INT,
+	inaltime DECIMAL,
+	diametru DECIMAL,
+	FOREIGN KEY (id) REFERENCES parcela (id)
+);
+
+CREATE TABLE IF NOT EXISTS sanatate_arbore
+(
+	id serial PRIMARY KEY,
+	stare VARCHAR (50),
+	data_verificare DATE,
+	observatii TEXT,
+	FOREIGN KEY (id) REFERENCES arbore (id)
+);
+
+CREATE TABLE IF NOT EXISTS interventie_silvica
+(
+	id serial PRIMARY KEY,
+	tip_interventie VARCHAR (50),
+	data_interventie DATE,
+	responsabil VARCHAR (100) NOT NULL,
+	id_arbore INT,
+	FOREIGN KEY (id) REFERENCES arbore (id)
+);
+
+CREATE TABLE IF NOT EXISTS angajat
+(
+	id serial PRIMARY KEY,
+	nume VARCHAR(100) NOT NULL,
+	functie VARCHAR (50),
+	data_angajare DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS angajat_interventie
+(
+	idangajat INT,
+	idinterventie INT,
+	PRIMARY KEY (idangajat, idinterventie),
+    FOREIGN KEY (idangajat) REFERENCES angajat(id),
+    FOREIGN KEY (idinterventie) REFERENCES interventie_silvica(id)
+);
